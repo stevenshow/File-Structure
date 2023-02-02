577ba366de49ffc80ee3678f16df76f9bd20c23e
Steven Schoebinger <steven.schoebinger@gmail.com>
2023-02-02 16:19:48 -0700

    formate index and app correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Error from "./pages/Error";
+import Login from "./pages/Login";
+import Page from "./layout/page";
+import Complete from "./pages/Complete";
+
 function App() {
-  return <div>This is the base app</div>;
+  const makePage = (Component) => {
+    return (
+      <Page>
+        <Component />
+      </Page>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: makePage(Home),
+      errorElement: <Error />,
+    },
+    {
+      path: "/Login",
+      element: makePage(Login),
+    },
+    {
+      path: "/Complete",
+      element: makePage(Complete),
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,38 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
-import Error from "./pages/Error";
-import Login from "./pages/Login";
-import Page from "./layout/page";
-import Complete from "./pages/Complete";
-
-const makePage = (Component) => {
-  return (
-    <Page>
-      <Component />
-    </Page>
-  );
-};
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: makePage(App),
-    errorElement: <Error />,
-  },
-  {
-    path: "/Login",
-    element: makePage(Login),
-  },
-  {
-    path: "/Complete",
-    element: makePage(Complete),
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
