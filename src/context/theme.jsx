16ac2676e7c9efdda125bb83416e25e4dcd62689
Steven Schoebinger <steven.schoebinger@gmail.com>
2023-02-03 08:13:37 -0700

    show context

---
+++ src/context/theme.jsx
@@ -0,0 +1,19 @@
+import React, { createContext, useState } from "react";
+
+export const ThemeContext = createContext();
+
+const ThemeContextProvider = (props) => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeContextProvider;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,14 @@
+import { useContext } from "react";
+import { ThemeContext } from "../../context/theme";
+
 export default function Home() {
-  return <div>Home page</div>;
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+  return (
+    <>
+      <div>Home page</div>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+      <div>Theme is set to: {theme}</div>
+    </>
+  );
 }
